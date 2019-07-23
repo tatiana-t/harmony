51197902ebdfc6fd3a4c0b84d1c8a16350091bcd
Tatiana <grabli_77@mail.ru>
2019-07-23 23:06:47 +0300

    set tonality scale

--- js/model.js
+++ js/model.js
@@ -1,38 +1,93 @@
-const majorSeq = [2, 2, 1, 2, 2, 2, 1];
-const minorSeq = [2, 1, 2, 2, 1, 2, 2];
-
 class Tonality {
-  constructor(key, harmony, type) {
+  constructor(key, harmon, type) {
     const halfToneAmount = 12;
-    //    const seq = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    const seq = [
-      //без дубль-диезов и дубль-бемолей
-      ['c', 'his'],
-      ['cis', 'des'],
-      ['d'],
-      ['dis', 'es'],
-      ['e', 'fes'],
-      ['f', 'eis'],
-      ['fis', 'ges'],
-      ['g'],
-      ['gis', 'as'],
-      ['a'],
-      ['ais', 'b'],
-      ['h']
-    ];
+    const harmony = {
+      major: {
+        sequence: [2, 2, 1, 2, 2, 2, 1],
+        steps: [
+          {
+            step: 1,
+            ht: 1
+  }, {
+            step: 2,
+            ht: 3
+  }, {
+            step: 3,
+            ht: 5
+  }, {
+            step: 4,
+            ht: 6
+  }, {
+            step: 5,
+            ht: 8
+  }, {
+            step: 6,
+            ht: 10
+  }, {
+            step: 7,
+            ht: 12
+  }
+]
+      },
+      minor: {
+        sequence: [2, 1, 2, 2, 1, 2, 2],
+        steps: [
+          {
+            step: 1,
+            ht: 1
+  }, {
+            step: 2,
+            ht: 3
+  }, {
+            step: 3,
+            ht: 4
+  }, {
+            step: 4,
+            ht: 6
+  }, {
+            step: 5,
+            ht: 8
+  }, {
+            step: 6,
+            ht: 9
+  }, {
+            step: 7,
+            ht: 11
+  }
+]
+      }
+    };
+
+    //    const seq = [
+    //      //без дубль-диезов и дубль-бемолей
+    //      ['c', 'his'],
+    //      ['cis', 'des'],
+    //      ['d'],
+    //      ['dis', 'es'],
+    //      ['e', 'fes'],
+    //      ['f', 'eis'],
+    //      ['fis', 'ges'],
+    //      ['g'],
+    //      ['gis', 'as'],
+    //      ['a'],
+    //      ['ais', 'b'],
+    //      ['h']
+    //    ];
     const names = ['c', 'd', 'e', 'f', 'g', 'a', 'h'];
-    this.major = [2, 2, 1, 2, 2, 2, 1];
-    this.minor = [2, 1, 2, 2, 1, 2, 2];
+
 
     //с какого полутона начинать строить гамму
-    const first = (key) => seq.reduce((acc, item, i) => {
-      if (item.indexOf(key) >= 0) {
-        return i;
-      }
-      return acc;
-    });
+    //    const first = (key) => names.reduce((acc, item, i) => {
+    //      if (item.indexOf(key) >= 0) {
+    //        return i + 1;
+    //      }
+    //      return acc;
+    //    });
+    const first = names.indexOf(key) + 1;
 
-    this.scaleSeq = (first) => this[harmony].reduce((acc, item, i) => {
+
+
+    const scaleSeq = (first) => harmony[harmon].sequence.reduce((acc, item, i) => {
       if (acc[i] + item >= halfToneAmount) {
         const delta = acc[i] + item - halfToneAmount;
         acc.push(delta);
@@ -42,73 +97,53 @@ class Tonality {
       return acc;
     }, [first]);
 
-    this.scale = () => {
-      return this.scaleSeq.map((item) => {
-        item = {
-          tone: item,
-          name: names[i]
+    const namedScale = (scale) => {
+      return scale.reduce((acc, item, i) => {
+        //        console.log(acc)
+        //        console.log('item',  item, 'i', i)
+        if (first + i >= 12) {
+          console.log(names[(first + i) - 12])
+          acc.push(item = {
+            tone: item,
+            name: names[(first + i) - 12],
+            step: i + 1
+          })
+          return acc;
         }
-      })
+        if ((first - 1) + i >= names.length) {
+          //          console.log(i)
+          acc.push(item = {
+            tone: item,
+            name: names[i - 3 - first - 1], //что это??? -3???,
+            step: i + 1
+          })
+          return acc;
+        }
+        acc.push(item = {
+          tone: item,
+          name: names[(first - 1) + i],
+          step: i + 1
+        })
+        return acc;
+      }, [])
     }
+    this.scale = namedScale(scaleSeq(first));
+    //    console.log(scaleSeq(first));
+    //    console.log(namedScale(scaleSeq(first)));
+
 
   }
 
 
 
 };
-const seq = [
-      //без дубль-диезов и дубль-бемолей
-      ['c', 'his'],
-      ['cis', 'des'],
-      ['d'],
-      ['dis', 'es'],
-      ['e', 'fes'],
-      ['f', 'eis'],
-      ['fis', 'ges'],
-      ['g'],
-      ['gis', 'as'],
-      ['a'],
-      ['ais', 'b'],
-      ['h']
-    ];
-const names = ['c', 'd', 'e', 'f', 'g', 'a', 'h'];
-const major = [2, 2, 1, 2, 2, 2, 1];
-const minor = [2, 1, 2, 2, 1, 2, 2];
-const first = (key, arr) => arr.reduce((acc, item, i) => {
-  //  console.log(item, key);
-  //  console.log('indexOf ', item.indexOf(key), i);
-  if (item.indexOf(key) >= 0) {
-    return i;
-  }
-  return acc;
-});
-const scale = (first) => major.reduce((acc, item, i) => {
-  if (acc[i] + item >= 12) {
-    const delta = acc[i] + item - 12;
-    acc.push(delta);
-    return acc;
-  }
-  acc.push(acc[i] + item);
-  return acc;
-}, [first]);
 
-const setNamedScale = (scale) => {
-  return scale.map((item, i) => {
-    if (first + i >= 12) {
-      console.log(names[(first + i) - 12])
-      return item = {
-        tone: item,
-        name: names[(first + i) - 12]
-      }
-    }
-    return item = {
-      tone: item,
-      name: names[first + i]
-    }
-  })
-}
 
-const test = first('d', seq);
-const testScale = scale(test);
-const namedScale = setNamedScale(testScale);
-console.log(test, testScale, namedScale);
+
+
+const tonalHarmony = new Tonality('d', 'major');
+console.log(tonalHarmony);
+//const test = first('d', seq);
+//const testScale = scale(test);
+//const namedScale = setNamedScale(testScale);
+//console.log(test, testScale, namedScale);
